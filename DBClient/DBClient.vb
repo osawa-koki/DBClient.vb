81021e3ad933c59f797f85c6ba7e0a7287e90831
osawa-koki <osawa-koki@osawa.dev>
2022-10-28 16:10:07 +0000

    列挙型と名前空間作った。

--- DBClient/DBClient.vb
+++ DBClient/DBClient.vb
@@ -1,69 +1,76 @@
 Imports System
 Imports System.Data.SqlClient
 
-Class DBClient
-  ' 接続文字列
-  Private Shared connection_string As String
-  ' SQL文
-  Private _sql As String = String.Empty
-  ' SQLパラメタ
-  Private _sqlParams As List(Of (arg As String, dbtype As SqlDbType, value As Object)) = New List(Of (arg As String, dbtype As SqlDbType, value As Object))
+Namespace DBMod
+  Public Enum DBMethod
+    DBSelect
+    DBSelectAll
+    DBExecute
+  End Enum
 
-  ' プログラムで一度だけ実行(初期化)
-  Sub Init(connection_string As String)
-    Me.connection_string = connection_string
-  End Sub
+  Class DBClient
+    ' 接続文字列
+    Private Shared connection_string As String
+    ' SQL文
+    Private _sql As String = String.Empty
+    ' SQLパラメタ
+    Private _sqlParams As List(Of (arg As String, dbtype As SqlDbType, value As Object)) = New List(Of (arg As String, dbtype As SqlDbType, value As Object))
 
-  ' SQL文の追加
-  Sub Add(sql As String)
-    Me._sql = " {sql} "
-  End Sub
+    ' プログラムで一度だけ実行(初期化)
+    Sub Init(connection_string As String)
+      Me.connection_string = connection_string
+    End Sub
 
-  ' SQLパラメタの追加
-  Sub AddParam(arg As String, dbtype As SqlDbType, value As Object)
-    _sqlParams.Add(arg, dbtype, value)
-  End Sub
+    ' SQL文の追加
+    Sub Add(sql As String)
+      Me._sql = " {sql} "
+    End Sub
 
-  Sub Main(args As String())
-    ' 関数内でのみ有効な変数を宣言
-    Dim myConn As SqlConnection
-    Dim myCmd As SqlCommand
-    Dim myReader As SqlDataReader
+    ' SQLパラメタの追加
+    Sub AddParam(arg As String, dbtype As SqlDbType, value As Object)
+      _sqlParams.Add(arg, dbtype, value)
+    End Sub
 
-    ' 接続文字列の設定がされていなかったら処理を終了
-    If Me.connection_string Is Nothing
-      Console.WriteLine("接続文字列が指定されていません。")
-      Return
-    End If
+    Sub Main(args As String())
+      ' 関数内でのみ有効な変数を宣言
+      Dim myConn As SqlConnection
+      Dim myCmd As SqlCommand
+      Dim myReader As SqlDataReader
 
-    ' 接続文字列からDBコネクションオブジェクトを生成
-    myConn = New SqlConnection(Me.connection_string)
+      ' 接続文字列の設定がされていなかったら処理を終了
+      If Me.connection_string Is Nothing
+        Console.WriteLine("接続文字列が指定されていません。")
+        Return
+      End If
 
-    ' コネクションからSQLコマンドオブジェクトを作成
-    myCmd = myConn.CreateCommand
+      ' 接続文字列からDBコネクションオブジェクトを生成
+      myConn = New SqlConnection(Me.connection_string)
 
-    ' SQLコマンド文に登録された文字列を指定
-    myCmd.CommandText = Me._sql
+      ' コネクションからSQLコマンドオブジェクトを作成
+      myCmd = myConn.CreateCommand
 
-    For Each item As (arg As String, dbtype As SqlDbType, value As Object) In Me._sqlParams
-      Dim key As String = item.arg
-      Dim dbtype As SqlDbType = item.dbtype
-      Dim value As Object = item.value
-      myCmd.Parameters.Add(key, dbtype).Value = value
-    Next
+      ' SQLコマンド文に登録された文字列を指定
+      myCmd.CommandText = Me._sql
 
-    ' コネクションをオープン
-    myConn.Open()
+      For Each item As (arg As String, dbtype As SqlDbType, value As Object) In Me._sqlParams
+        Dim key As String = item.arg
+        Dim dbtype As SqlDbType = item.dbtype
+        Dim value As Object = item.value
+        myCmd.Parameters.Add(key, dbtype).Value = value
+      Next
 
-    ' カーソルを取得
-    myReader = myCmd.ExecuteReader()
+      ' コネクションをオープン
+      myConn.Open()
 
-    Do While myReader.Read()
-      results = results & myReader.GetString(0) & vbTab & myReader.GetString(1) & vbLf
-    Loop
+      ' カーソルを取得
+      myReader = myCmd.ExecuteReader()
 
-    Console.WriteLine(results)
-    myReader.Close()
-    myConn.Close()
-  End Sub
-End Class
+      Do While myReader.Read()
+        results = results & myReader.GetString(0) & vbTab & myReader.GetString(1) & vbLf
+      Loop
+
+      myReader.Close()
+      myConn.Close()
+    End Sub
+  End Class
+End Namespace
