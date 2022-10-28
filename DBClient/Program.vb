a4b2ec1f7be90fd85a95077ea5c2e1e58c873e36
osawa-koki <osawa-koki@osawa.dev>
2022-10-28 17:14:39 +0000

    テスト用モジュール作った。

--- DBClient/Program.vb
+++ DBClient/Program.vb
@@ -1,8 +1,14 @@
 Imports System
-Imports DBMod
 
 Module Program
   Sub Main(args As String())
-    DBClient.Init("Persist Security Info=False;User ID=sa;Password=P@ssw0rd;Initial Catalog=master;Server=127.0.0.1;Encrypt=False;")
+    DBClient.Init("Persist Security Info=False;User ID=sa;Password=P@ssw0rd;Initial Catalog=pokemon;Server=127.0.0.1;Encrypt=False;")
+    Dim client As DBClient = New DBClient()
+
+    client.Add("SELECT number, name")
+    client.Add("FROM pokemon")
+    Dim result As List (Of Dictionary(Of String, Object)) = client.DBSelectAll()
+
+    Console.WriteLine(result)
   End Sub
 End Module
